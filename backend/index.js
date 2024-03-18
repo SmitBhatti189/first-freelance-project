38730c32d927623776c4070a72781f07dc6aec0b
shrutsureja <shrutsureja.code@gmail.com>
2024-03-18 19:24:40 +0530

    added the API routes featuredproducts, product/:productID, categories

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,15 @@
 import express from 'express';
 import { PORT } from './config/index.js';
-import pool from './models/db.js';
 import cors from 'cors';
 import user from './routes/user.js';
 import products from './routes/products.js';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api',user);
 app.use('/api',products);
-
-// app.get('/users', async (req, res) => {
-//     let connection;
-//     try {
-//         connection = await pool.getConnection();
-//         const results = await connection.query('SELECT * FROM users');
-//         res.json(results);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send('Error fetching users');
-//     } finally {
-//         if (connection) connection.release();
-//     }
-// });
 
 
 app.listen(PORT, () => {
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { tokenValidation } from '../middleware/tokenValidation.js';
 import pool from '../models/db.js';
 import { jsonResponse } from '../utils/responseSchema.js';
+import { productValidation } from '../middleware/productValidation.js';
 const router = Router();
 
 // all Products API
@@ -10,6 +10,68 @@ router.get('/products', async (req, res) => {
     try {
         connection = await pool.getConnection();
         const results = await connection.query('SELECT * FROM products');
+        res.json(jsonResponse(200, 'OK', results));
+    } catch (error) {
+        console.error(error);
+        res.json(jsonResponse(500, 'Internal Server Error', error, true));
+    }
+    finally {
+        if(connection) connection.release();
+    }
+});
+
+// featured Products API
+router.get('/featuredproducts', async (req, res) => {
+    let { nProducts } = req.query;
+    nProducts = parseInt(nProducts);
+    if (typeof nProducts !== 'number' || isNaN(nProducts)){
+        nProducts = 5;
+    }
+    nProducts = nProducts > 5 ? nProducts : 5;
+    nProducts = nProducts > 10 ? 10 : nProducts;
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const results = await connection.query('SELECT * FROM products');
+        // random n products or if n is null then 5 products
+        if(nProducts > results.length) nProducts = results.length;
+        const randomProducts = results.sort(() => Math.random() - Math.random()).slice(0, nProducts);
+        res.json(jsonResponse(200, 'OK', randomProducts));
+    } catch (error) {
+        console.error(error);
+        res.json(jsonResponse(500, 'Internal Server Error', error, true));
+    }
+    finally {
+        if(connection) connection.release();
+    }
+});
+
+router.get('/product/:productID', productValidation, async (req, res) => {
+    const { productID } = req.params;
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const result = await connection.query('SELECT * FROM products WHERE product_id = ?', [productID]);
+        res.json(jsonResponse(200, 'OK', result[0]));
+    } catch (error) {
+        console.error(error);
+        res.json(jsonResponse(500, 'Internal Server Error', error, true));
+    }
+    finally {
+        if(connection) connection.release();
+    }
+});
+
+router.get('/categories', async (req, res) => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const results = await connection.query('SELECT * FROM category');
+        // for each category get any one product image url
+        for(let i = 0; i < results.length; i++) {
+            const product = await connection.query('SELECT * FROM products WHERE category = ? ', [results[i].categoryID]);
+            results[i].image_url = product[Math.floor(Math.random() * product.length)].image_url;
+        }
         res.json(jsonResponse(200, 'OK', results));
     } catch (error) {
         console.error(error);
